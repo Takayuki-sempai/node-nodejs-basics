4416c21d928eb368029e7f09519a9ab0ac34a8c6
Vasiliy Prokofiev <Takayuki.sempai@gmail.com>
2024-10-12 22:55:23 +0300

    feat: implement rename file task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,17 @@
+import {rename as renameFile} from "node:fs/promises";
+import {join} from "path";
+
+const dirname = import.meta.dirname
+const filesPath = join(dirname, 'files')
+const oldFileName = join(filesPath, 'wrongFilename.txt')
+const newFileName = join(filesPath, 'properFilename.md')
+
 const rename = async () => {
-    // Write your code here 
+    try {
+        await renameFile(oldFileName, newFileName);
+    } catch (err) {
+        throw new Error("FS operation failed")
+    }
 };
 
 await rename();
