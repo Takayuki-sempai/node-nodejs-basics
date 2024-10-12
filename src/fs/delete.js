e55acd527a70d2c701000c2b4e420cc1327fdb68
Vasiliy Prokofiev <Takayuki.sempai@gmail.com>
2024-10-12 23:18:23 +0300

    feat: implement delete file task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
+import {rm} from "fs/promises";
+import {join} from "path";
+
+const dirname = import.meta.dirname
+const filepath = join(dirname, "files", "fileToRemove.txt")
+
 const remove = async () => {
-    // Write your code here 
+    try {
+        await rm(filepath);
+    } catch (err) {
+        throw new Error("FS operation failed")
+    }
 };
 
 await remove();
