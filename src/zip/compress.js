1dbebfee7d24928ead8439bdc02ee4ce52cdf20d
Vasiliy Prokofiev <Takayuki.sempai@gmail.com>
2024-10-13 02:15:50 +0300

    feat: implement zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import fs from "fs";
+import {pipeline} from "node:stream";
+import {join} from "path";
+import {createGzip} from "node:zlib";
+
+const dirname = import.meta.dirname
+const fileName = join(dirname, "files", "fileToCompress.txt")
+const compressFileName = join(dirname, "files", "archive.gz")
+
 const compress = async () => {
-    // Write your code here 
+    pipeline(
+        fs.createReadStream(fileName),
+        createGzip(),
+        fs.createWriteStream(compressFileName),
+        (err) => {
+            if (err) throw new Error(err)
+        }
+    )
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import {pipeline} from "node:stream";
+import fs from "fs";
+import {createGunzip} from "node:zlib";
+import {join} from "path";
+
+const dirname = import.meta.dirname
+const fileName = join(dirname, "files", "archive.gz")
+const decompressFileName = join(dirname, "files", "fileToCompress.txt")
+
 const decompress = async () => {
-    // Write your code here 
+    await pipeline(
+        fs.createReadStream(fileName),
+        createGunzip(),
+        fs.createWriteStream(decompressFileName),
+        (err) => {
+            if (err) throw new Error(err)
+        }
+    )
 };
 
 await decompress();
