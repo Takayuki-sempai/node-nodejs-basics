8c9fddbf65916a18d825040d29e1c459d6e25148
Vasiliy Prokofiev <Takayuki.sempai@gmail.com>
2024-10-12 23:27:09 +0300

    feat: implement list files task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+import {join} from "path";
+import {readdir} from "node:fs/promises";
+
+const dirname = import.meta.dirname
+const filesPath = join(dirname, 'files')
+
 const list = async () => {
-    // Write your code here 
+    try {
+        const files = await readdir(filesPath)
+        console.log(files)
+    } catch (err) {
+        throw new Error("FS operation failed")
+    }
 };
 
 await list();
