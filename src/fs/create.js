91b9dccd86769e7c02fb5ebe323cd4b0bd1a615e
Vasiliy Prokofiev <Takayuki.sempai@gmail.com>
2024-10-12 22:35:11 +0300

    feat: implement create file task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,15 @@
+import {join} from "path";
+import {writeFile} from "node:fs/promises";
+
+const dirname = import.meta.dirname
+const filepath = join(dirname, "files", "fresh.txt")
+
 const create = async () => {
-    // Write your code here 
+    try {
+        await writeFile(filepath, "I am fresh and young", {encoding: "utf8", flag: "wx"});
+    } catch (err) {
+        throw new Error("FS operation failed")
+    }
 };
 
 await create();
