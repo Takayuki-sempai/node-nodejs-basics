1b3d6c78c0032587c630a0d3c18eb3174d9521cf
Vasiliy Prokofiev <Takayuki.sempai@gmail.com>
2024-10-13 00:38:54 +0300

    feat: implement read streams task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,12 @@
+import fs from "fs";
+import {join} from "path";
+
+const dirname = import.meta.dirname
+const filepath = join(dirname, "files", "fileToRead.txt")
+
 const read = async () => {
-    // Write your code here 
+    const rs = fs.createReadStream(filepath);
+    rs.pipe(process.stdout);
 };
 
 await read();
