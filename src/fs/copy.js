9bb1c2ea3bab96b1f5e4bb024ae1f8953cb0b0c6
Vasiliy Prokofiev <Takayuki.sempai@gmail.com>
2024-10-12 22:46:11 +0300

    feat: implement copy file task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,31 @@
+import {join} from "path";
+import {readdir, mkdir, copyFile} from "node:fs/promises";
+
+const dirname = import.meta.dirname
+const filesPath = join(dirname, 'files')
+const filesCopyPath = join(dirname, 'files_copy')
+
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        const [files] = await Promise.all([
+            readdir(filesPath),
+            mkdir(filesCopyPath)
+        ]);
+
+        await Promise.all(
+            files.map(async file => {
+                const source = join(filesPath, file);
+                const destination = join(filesCopyPath, file);
+                await copyFile(source, destination);
+            })
+        )
+
+        //Alternative
+        //await cp(filesPath, filesCopyPath, {recursive: true});
+    } catch (err) {
+        throw new Error("FS operation failed")
+    }
 };
 
 await copy();
