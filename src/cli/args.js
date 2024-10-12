f9628bbc5c4cc6a42c76ffde59a08de20b6c525c
Vasiliy Prokofiev <Takayuki.sempai@gmail.com>
2024-10-13 00:04:00 +0300

    feat: implement args task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+    const argsObject = []
+    for(let i = 2; i < process.argv.length; i+=2) {
+        argsObject[process.argv[i]] = process.argv[i + 1]
+    }
+    const args = Object.entries(argsObject)
+        .map(([key, value]) =>`${key.slice(2)} is ${value}`)
+    console.log(args.join(', '));
 };
 
 parseArgs();
