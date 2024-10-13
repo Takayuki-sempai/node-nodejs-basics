4edc814fca9917049d7acf686a502f62f57c0d5f
Vasiliy Prokofiev <Takayuki.sempai@gmail.com>
2024-10-13 03:15:59 +0300

    feat: implement worker task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,31 @@
+import os from "node:os";
+import {Worker} from "node:worker_threads"
+
+const workerPath = new URL("./worker.js", import.meta.url);
+
 const performCalculations = async () => {
-    // Write your code here
+    const cores = os.cpus().length;
+    const result = []
+    const workers = []
+
+    for (let i = 0; i < cores; i++) {
+        const worker = new Worker(workerPath, {workerData: 10 + i})
+
+        worker.on("message", (data) => {
+            result.push({status: "resolved", data })
+        })
+
+        worker.on("error", (data) => {
+            result.push({status: "error", data: null })
+        })
+
+        workers.push(worker)
+    }
+
+    await Promise.allSettled(
+        workers.map(worker => new Promise(resolve => worker.on("exit", resolve)))
+    );
+    console.log(result)
 };
 
 await performCalculations();
