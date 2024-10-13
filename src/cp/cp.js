54569623deb517f4a3efd4f7a52359ba6df732d6
Vasiliy Prokofiev <Takayuki.sempai@gmail.com>
2024-10-13 03:35:19 +0300

    feat: implement fork task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,15 @@
+import {fork} from 'child_process';
+import {join} from "path";
+
+const dirname = import.meta.dirname
+const filepath = join(dirname, "files", "script.js")
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    fork(filepath, args)
+    //const childProcess = fork(filepath, args, {silent: true})
+    // process.stdin.pipe(childProcess.stdin)
+    // childProcess.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ["arg1", "arg2", "33"]);
