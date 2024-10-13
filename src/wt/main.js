6eaedd344d9b816ddf3fd98694624fa259bc36a2
Vasiliy Prokofiev <Takayuki.sempai@gmail.com>
2024-10-13 03:40:15 +0300

    fix: result order for worker task

--- src/wt/main.js
+++ src/wt/main.js
@@ -5,18 +5,18 @@ const workerPath = new URL("./worker.js", import.meta.url);
 
 const performCalculations = async () => {
     const cores = os.cpus().length;
-    const result = []
+    const result = [cores]
     const workers = []
 
     for (let i = 0; i < cores; i++) {
         const worker = new Worker(workerPath, {workerData: 10 + i})
 
         worker.on("message", (data) => {
-            result.push({status: "resolved", data })
+            result[i] = {status: "resolved", data }
         })
 
         worker.on("error", (data) => {
-            result.push({status: "error", data: null })
+            result[i] = {status: "error", data: null }
         })
 
         workers.push(worker)
